b40c5a34e88ba18b56d92f220aa9e0175f241eef
John Meyer <jmeyer5@nd.edu>
2017-08-30 14:07:04 -0400

    Update ~update.php

--- ~update.php
+++ ~update.php
@@ -1,4 +1,4 @@
 <?php
 echo("<ul><li>");
-echo(`../dunne.sh | tr '\n' '&'| sed 's:&&:<\/li><\/ul><br/><ul><li>:g' | sed 's:&:<\/li><li>:g' `); //calls the update script stored on the server
+echo(`../dunne.sh | tr '\n' '&'| sed 's:&&:<\/li><\/ul><hr/><ul><li>:g' | sed 's:&:<\/li><li>:g' `); //calls the update script stored on the server
 echo("</li></ul>");
