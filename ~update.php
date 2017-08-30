1fb619ee8a32cc3c7666bec0f1058f8800962d76
John Meyer <jmeyer5@nd.edu>
2017-08-30 14:10:31 -0400

    Returned formatting to dunne script

--- ~update.php
+++ ~update.php
@@ -1,4 +1,2 @@
 <?php
-echo("<ul><li>");
-echo(`../dunne.sh | tr '\n' '&'| sed 's:&&:<\/li><\/ul><hr/><ul><li>:g' | sed 's:&:<\/li><li>:g' `); //calls the update script stored on the server
-echo("</li></ul>");
+echo(`../dunne.sh`); //calls the update script stored on the server
