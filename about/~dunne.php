2d111a8d822770773e1b2b4bc43661249b913a20
Jack Meyer <jack10042@gmail.com>
2017-08-31 16:53:59 -0400

    Little col fix

--- about/~dunne.php
+++ about/~dunne.php
@@ -16,19 +16,21 @@
 		<div class="row-fluid">
 			<div class="col-md-10 col-md-offset-1" style="background-color: #fff">
 				<div class="row-fluid">
-					<div class="text-center">
-						<img class="img-rounded" src="../img/people/dunne.jpg" />
+					<div class="col-md-10 col-md-offset-1">
+						<div class="text-center">
+							<img class="img-rounded" src="../img/people/dunne.jpg" />
+						</div>
+						<h1 class="text-center">Jimmy Dunne</h1>
+						<p class="indented">
+							The men of Dunne are forever indebted to Mr. James Dunne III and his wife Susan. Mr. Dunne donated $20 million to the University to construct Dunne Hall, which was opened in the fall of 2016. Mr. Dunne has previously supported several other Notre Dame initiatives, most notably the Ann F. Dunne and Elizabeth M. Riley Science Director of the Harper Cancer Research Institute. “It is with great pride and appreciation for Notre Dame that we provide this support,” said Mr. Dunne. “The roommates I met freshman year in Alumni Hall have been friends for life. It is my hope that these new residence halls offer the same opportunity for every student who passes through them.” The rectorship of Dunne Hall, now headed by Fr. Matthew Kuczora, C.S.C. is named after these roommates: John Coyne, Jimmy Martin, Rich Riley and Stan Ziherl, all members of the Class of 1978. Having graduated from Notre Dame with a BA in Economics, Mr. Dunne also serves on the University’s Board of Trustees.
+						</p>
+						<p class="indented">
+							Mr. Dunne is currently Senior Managing Principal of Sandler O’Neill + Partners, L.P., and heads the firm’s Executive Committee. Mr. Dunne has spent over 30 years working in New York’s financial sector, and is a frequent contributor to media outlets such as Bloomberg and MSNBC. Notably, Mr. Dunne, spearheaded the firm’s recovery process from the ashes of September 11th. Sandler O’Neill lost 68 of its 171 partners and employees in the World Trade Center attacks, including Herman Sandler and Chris Quackenbush, two of the three executives who ran the firm. Mr. Dunne assisted in providing for the bereaved families and set the tone for the firm’s determination to succeed.
+						</p>
+						<p class="indented">
+							Mr. Dunne and his roommates also help to support Dunne Hall’s annual Moreau Day Dinner, when the Men of Dunne come together to honor the patron of Dunne Hall’s chapel (and founder of the Congregation of Holy Cross), Blessed Basil Moreau. An avid golfer himself, Mr. Dunne has also helped to support and outfit Dunne’s interhall athletic teams. 
+						</p>
 					</div>
-					<h1 class="text-center">Jimmy Dunne</h1>
-					<p class="indented">
-						The men of Dunne are forever indebted to Mr. James Dunne III and his wife Susan. Mr. Dunne donated $20 million to the University to construct Dunne Hall, which was opened in the fall of 2016. Mr. Dunne has previously supported several other Notre Dame initiatives, most notably the Ann F. Dunne and Elizabeth M. Riley Science Director of the Harper Cancer Research Institute. “It is with great pride and appreciation for Notre Dame that we provide this support,” said Mr. Dunne. “The roommates I met freshman year in Alumni Hall have been friends for life. It is my hope that these new residence halls offer the same opportunity for every student who passes through them.” The rectorship of Dunne Hall, now headed by Fr. Matthew Kuczora, C.S.C. is named after these roommates: John Coyne, Jimmy Martin, Rich Riley and Stan Ziherl, all members of the Class of 1978. Having graduated from Notre Dame with a BA in Economics, Mr. Dunne also serves on the University’s Board of Trustees.
-					</p>
-					<p class="indented">
-						Mr. Dunne is currently Senior Managing Principal of Sandler O’Neill + Partners, L.P., and heads the firm’s Executive Committee. Mr. Dunne has spent over 30 years working in New York’s financial sector, and is a frequent contributor to media outlets such as Bloomberg and MSNBC. Notably, Mr. Dunne, spearheaded the firm’s recovery process from the ashes of September 11th. Sandler O’Neill lost 68 of its 171 partners and employees in the World Trade Center attacks, including Herman Sandler and Chris Quackenbush, two of the three executives who ran the firm. Mr. Dunne assisted in providing for the bereaved families and set the tone for the firm’s determination to succeed.
-					</p>
-					<p class="indented">
-						Mr. Dunne and his roommates also help to support Dunne Hall’s annual Moreau Day Dinner, when the Men of Dunne come together to honor the patron of Dunne Hall’s chapel (and founder of the Congregation of Holy Cross), Blessed Basil Moreau. An avid golfer himself, Mr. Dunne has also helped to support and outfit Dunne’s interhall athletic teams. 
-					</p>
 				</div>
 			</div>
 		</div>
