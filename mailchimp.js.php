2c949726cf7fde4eb9d6bcafc6a83709202aae46
Jack Meyer <jack10042@gmail.com>
2017-08-31 12:47:02 -0400

    Removed a random slash

--- mailchimp.js.php
+++ mailchimp.js.php
@@ -1,5 +1,5 @@
 <script type="text/javascript">
-$.get("<?php echo $root; ?>/etc/mailchimp_rss.php",function(data){
+$.get("<?php echo $root; ?>etc/mailchimp_rss.php",function(data){
 	$("#mailchimp").html($($.parseXML(data)).find("description:eq(1)").html().replace(/^[\s\S]*?<body(.*?)>([\s\S]*?)<\/body>[\s\S]*?$/,"<div$1>$2</div>"));
 	$(window).resize();
 })
