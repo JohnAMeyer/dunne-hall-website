d13f917d2e41dd6f2c258a5b8de05614401d8459
Jack Meyer <jack10042@gmail.com>
2017-09-04 18:06:30 -0400

    Extract css

--- includes/head.php
+++ includes/head.php
@@ -2,8 +2,10 @@
 $root = '/';
 if(strpos(dirname(__FILE__),'DunneHallWebsite')!== false)
 	$root = '/dunne/';
-$description = "Dunne Hall is Notre Dame's newest all-male dorm, located on Mod/East Quad.";
-$title = "Dunne Hall";
+if(!isset($description))
+	$description = "Dunne Hall is Notre Dame's newest all-male dorm, located on Mod/East Quad.";
+if(!isset($title))
+	$title = "Dunne Hall";
 ?>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,14 +15,13 @@ $title = "Dunne Hall";
 <meta name="apple-mobile-web-app-title" content="<?php echo $title; ?>">
 <meta name="application-name" content="Dunne Hall">
 <meta name="twitter:card" content="summary">
-<meta name="twitter:site" content="@notredame">
+<!--meta name="twitter:site" content="@notredame"-->
 <!--meta name="twitter:creator" content="@ndwebteam"-->
 <meta property="og:site_name" content="Dunne Hall">
 <meta property="og:title" content="Dunne Hall"> <!-- set to h1 content? -->
 <meta property="og:description" content="<?php echo $description; ?>">
 <meta property="og:type" content="website">
-<meta property="og:url" content="http://onmessage.nd.edu/university-branding/website-requirements/color-requirements/">
-<meta property="og:image" content="http://onmessage.nd.edu//images/webclips/nd-webclip-144.png"> <!-- TODO -->
+<meta property="og:image" content="<?php echo $root; ?>img/favicon/apple-touch-icon-180x180.png">
 <meta property="og:locale" content="en_US">
 
 <title><?php echo $title; ?></title>
@@ -42,124 +43,5 @@ $title = "Dunne Hall";
 
 <script type="text/javascript" async="" id="ndalertbarscript" src="//emergency.nd.edu/api/alert/"></script>
 
-<style type="text/css">
-
-	@media (min-width: 768px) {
-		.navbar-header {
-			width: 20%;
-		}
-		.nav > li {
-			width:25%
-		}
-	}
-	.nav.navbar-nav {
-		width: 80%;
-	}
-	@media (min-width: 990px) {
-		#gallery {
-			padding: 0 10px;
-		}
-	}
-	@media (max-width: 767px) {
-		.nav > .dropdown-menu a::before {
-			content: "• ";
-		}
-	}
-	@media (max-width: 1199px) and (min-width: 768px) {
-		nav.navbar{
-			border-radius: 0px;
-		}
-		div.nav-parent {
-			padding-left: 0px;
-			padding-right: 0px;
-		}
-	}
-	.row-fluid > .text-center > img {
-		margin-top: 10px;
-	}
-	a {
-		color: #0edd0e;
-	}
-	.home-link {
-		cursor: pointer;
-	}
-	@media (max-width: 990px){
-		#gallery {
-			margin-top: 10px;
-		}
-	}
-	body {
-		background-color: #003a79;
-	}
-	@media(max-width:767px){
-		.nav-parent{
-			padding:0px;
-		}
-	}
-	footer {
-		background-color: #fff;
-		color:#666;
-	}
-	footer a:active {
-		color: #aa272f;
-	}
-	footer a:hover {
-		color: #002b5b;
-	}
-	footer a:visited {
-		color: #4a3651;
-	}
-	footer a {
-		color: #004da3;
-		outline: none;
-		-webkit-tap-highlight-color: rgba(0,0,0,0);
-	}
-	@media(max-width: 990px){
-		footer{
-			text-align: center;
-		}
-	}
-	h1 {
-		margin: 25px;
-	}
-	.navbar {
-		border-radius: 0px;
-	}
-	body {
-		font-family: "Helvetica Neue", Helvetica, Arial, Verdana, sans-serif;
-	}
-	#float-photo {
-		position: absolute;
-		z-index: 50;
-		width: 17vw;
-		left:6.5vw;
-		top:10vw;
-	}
-	@media(max-width: 1800px){
-		#float-photo {
-			left: 4vw;
-			top: 14vw;
-		}
-	}
-	@media(min-width: 1950px){
-		#gallery {
-			width: 76vw;
-		}
-	}
-	@font-face {
-    	font-family: hudsonNY;
-    	src: url(<?php echo $root; ?>etc/HudsonNYSerif.ttf);
-    	src: url(<?php echo $root; ?>etc/HudsonNYSerif.otf);
-	}
-	.hudson {
-		font-family: hudsonNY;
-	}
-	.navbar-default .navbar-nav>li>a{
-		color:#282646;
-		font-weight: bold;
-	}
-	h1 {
-		font-family: hudsonNY;
-	}
-</style>
-<link rel="stylesheet" type="text/css" href="<?php echo $root ?>styles/ndbrand.css""/>
+<link rel="stylesheet" type="text/css" href="<?php echo $root ?>styles/override.css.php"/>
+<link rel="stylesheet" type="text/css" href="<?php echo $root ?>styles/ndbrand.css"/>
---
+++ styles/override.css.php
@@ -0,0 +1,123 @@
+<?php
+$root = '/';
+if(strpos(dirname(__FILE__),'DunneHallWebsite')!== false)
+	$root = '/dunne/';
+header('Content-Type: text/css');
+?>
+@media (min-width: 768px) {
+	.navbar-header {
+		width: 20%;
+	}
+	.nav > li {
+		width:25%
+	}
+}
+.nav.navbar-nav {
+	width: 80%;
+}
+@media (min-width: 990px) {
+	#gallery {
+		padding: 0 10px;
+	}
+}
+@media (max-width: 767px) {
+	.nav > .dropdown-menu a::before {
+		content: "• ";
+	}
+}
+@media (max-width: 1199px) and (min-width: 768px) {
+	nav.navbar{
+		border-radius: 0px;
+	}
+	div.nav-parent {
+		padding-left: 0px;
+		padding-right: 0px;
+	}
+}
+.row-fluid > .text-center > img {
+	margin-top: 10px;
+}
+a {
+	color: #0edd0e;
+}
+.home-link {
+	cursor: pointer;
+}
+@media (max-width: 990px){
+	#gallery {
+		margin-top: 10px;
+	}
+}
+body {
+	background-color: #003a79;
+}
+@media(max-width:767px){
+	.nav-parent{
+		padding:0px;
+	}
+}
+footer {
+	background-color: #fff;
+	color:#666;
+}
+footer a:active {
+	color: #aa272f;
+}
+footer a:hover {
+	color: #002b5b;
+}
+footer a:visited {
+	color: #4a3651;
+}
+footer a {
+	color: #004da3;
+	outline: none;
+	-webkit-tap-highlight-color: rgba(0,0,0,0);
+}
+@media(max-width: 990px){
+	footer{
+		text-align: center;
+	}
+}
+h1 {
+	margin: 25px;
+}
+.navbar {
+	border-radius: 0px;
+}
+body {
+	font-family: "Helvetica Neue", Helvetica, Arial, Verdana, sans-serif;
+}
+#float-photo {
+	position: absolute;
+	z-index: 50;
+	width: 17vw;
+	left:6.5vw;
+	top:10vw;
+}
+@media(max-width: 1800px){
+	#float-photo {
+		left: 4vw;
+		top: 14vw;
+	}
+}
+@media(min-width: 1950px){
+	#gallery {
+		width: 76vw;
+	}
+}
+@font-face {
+	font-family: hudsonNY;
+	src: url(<?php echo $root; ?>etc/HudsonNYSerif.ttf);
+	src: url(<?php echo $root; ?>etc/HudsonNYSerif.otf);
+}
+.hudson {
+	font-family: hudsonNY;
+}
+.navbar-default .navbar-nav>li>a{
+	color:#282646;
+	font-weight: bold;
+}
+h1 {
+	font-family: hudsonNY;
+}
